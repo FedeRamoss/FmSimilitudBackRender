08d44e51b563713ca77d7df4d04aeaeef7b073e0
FedeRamoss <fedegonzaloramos@gmail.com>
2026-04-14 20:34:19 -0300

    Commit para deploy de vercel

--- api/index.py
+++ api/index.py
@@ -10,7 +10,7 @@ app = FastAPI(title="FM Similarity API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://fm-similitud-back.vercel.app"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
